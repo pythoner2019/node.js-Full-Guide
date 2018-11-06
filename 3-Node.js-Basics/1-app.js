0a8d4a5c988500a22f869525286d16b611e6018a
Your Name <you@example.com>
2018-11-06 23:13:45 +0200

    Final view + add routes.js file which contain routes of url

--- 3-Node.js-Basics/1-app.js
+++ 3-Node.js-Basics/1-app.js
@@ -1,69 +1,16 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-	const url = req.url;
-	const method = req.method;
 
-	if (url === '/') {
-		res.write(`
-			<html>
-				<head>
-					<title>Enter Message</title>
-				</head>
-				<body>
-					<form action="/message" method="POST">
-						<input type="text" name="message">
-						<button type="submit">
-							Send Message Please...
-						</button>
-					</form>	
-				</body>
-			</html>
-		`)
-		return res.end();
-	}
 
-	if (url === '/message' && method === 'POST') {
-		const body = [];
-
-		req.on('data', (chunk) => {
-			console.log(chunk);
-			body.push(chunk);
-		});
-
-		req.on('end', () => {
-			const parsedBody = Buffer.concat(body).toString();
-			const message = parsedBody.split('=')[1];
-			console.log(message);
-			fs.writeFileSync('message.txt', message);
-		});
-		
-		res.statusCode = 302;
-		res.setHeader('Location', '/');
-		return res.end();
-	};
-	
-	res.setHeader('Content-Type', 'text/html');
-	res.write(`
-		<html>
-			<head>
-				<title>First Page</title>
-			</head>
-			<body>
-				<h1>Hello from server</h1>
-				<h3>Nice love story</h3>
-			</body>
-		</html>
-	`);
-	res.end();
-})
+const server = http.createServer(routes.handler);
 
 server.listen(3000, () => {
 	console.log('port listening now!');
 });
 
 
+console.log(routes.someText);
 
 
 
