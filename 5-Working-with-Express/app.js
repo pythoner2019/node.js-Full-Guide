04c25361f8ac3173fc59aa81c9a3aadff763a4db
Your Name <you@example.com>
2018-11-11 11:51:31 +0200

    work with express part 1 final

--- 5-Working-with-Express/app.js
+++ 5-Working-with-Express/app.js
@@ -1,22 +1,21 @@
+const path = require('path');
+
 const express = require('express');
-const bodyParser = require('body-parser');      // for req.body
+const bodyParser = require('body-parser');
+
 const app = express();
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes  = require('./routes/shop');
-
-
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(adminRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-
-
-
-
-const port = 3000;
-app.listen(port, () => {
-	console.log(`Server started on port: ${port}`);
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
+app.listen(3000);
