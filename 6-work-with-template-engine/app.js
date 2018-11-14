cfc5fd87c3fb769c7fd4daab92a0f7e10bd49024
Your Name <you@example.com>
2018-11-14 21:37:15 +0200

    project with PUG template engine

--- 6-work-with-template-engine/app.js
+++ 6-work-with-template-engine/app.js
@@ -3,19 +3,28 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+// setting template engine:
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+// calling routes:
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 
-
+// Using middleware:
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', { pageTitle: 'Error Page', message: 'Page Not Found' })
 });
 
+
+
+
+// App listen:
 app.listen(3000);
