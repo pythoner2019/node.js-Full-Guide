5e394150005091b2e573ec9f3ae42a833a8a3eda
Your Name <you@example.com>
2018-11-19 17:43:26 +0200

    Final project with handlebars template engine

--- 6-work-with-template-engine/app.js
+++ 6-work-with-template-engine/app.js
@@ -1,15 +1,26 @@
 const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
+const expressHbs = require('express-handlebars');
 const app = express();
 
 // setting template engine:
-app.set('view engine', 'pug');
+app.engine(
+    'handlebars', 
+    expressHbs({
+        layoutsDir: 'views/layouts/', 
+        defaultLayout: 'main-layout',
+        extname: 'handlebars'
+    })
+);
+app.set('view engine', 'handlebars');
 app.set('views', 'views');
 
 // calling routes:
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+
 
 
 // Using middleware:
