ac44221936457d6d4027c321ab08ea8972116ab7
Your Name <you@example.com>
2018-11-19 21:46:50 +0200

    Final Project with "ejs" - template engine

--- 6-work-with-template-engine/app.js
+++ 6-work-with-template-engine/app.js
@@ -1,26 +1,15 @@
 const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
-const expressHbs = require('express-handlebars');
 const app = express();
 
 // setting template engine:
-app.engine(
-    'handlebars', 
-    expressHbs({
-        layoutsDir: 'views/layouts/', 
-        defaultLayout: 'main-layout',
-        extname: 'handlebars'
-    })
-);
-app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 // calling routes:
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
-
 
 
 // Using middleware:
@@ -33,8 +22,6 @@ app.use(shopRoutes);
 app.use((req, res, next) => {
     res.status(404).render('404', { pageTitle: 'Error Page', message: 'Page Not Found' })
 });
-
-
 
 
 // App listen:
